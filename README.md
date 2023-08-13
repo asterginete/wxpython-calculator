befc21b3462cf4141573bffd4533956b9fe987bd
asterginete <asterginete@yahoo.com>
2023-08-14 00:08:53 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# wxpython-calculator
-A simple calculator app with wxPython
+Certainly! Here's a README file for the simple calculator app:
+
+---
+
+# Simple Calculator App
+
+A basic calculator application built using Python and wxPython, inspired by the Windows calculator.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Installation
+
+### Prerequisites
+
+- Python 3.x
+- pip (Python package installer)
+
+### Steps
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/yourusername/simple-calculator.git
+cd simple-calculator
+```
+
+2. Install the required packages:
+
+```bash
+pip install wxPython
+```
+
+3. Run the calculator:
+
+```bash
+python main.py
+```
+
+## Usage
+
+1. Launch the calculator by running `python main.py`.
+2. Use the numerical buttons to input numbers.
+3. Use the arithmetic operation buttons (`+`, `-`, `*`, `/`) for calculations.
+4. Press `=` to see the result.
+5. Use the `C` button to clear the display.
+
+## Features
+
+- Basic arithmetic operations: addition, subtraction, multiplication, and division.
+- Clear functionality to reset calculations.
+- Intuitive GUI similar to the Windows calculator.
+
+## Contribute
+
+Contributions are welcome! Please fork the repository and open a pull request with your changes, or open an issue to discuss a bug or suggest a new feature.
---
+++ main.py
@@ -0,0 +1,53 @@
+import wx
+
+class Calculator(wx.Frame):
+    def __init__(self, parent, title):
+        super(Calculator, self).__init__(parent, title=title, size=(300, 400))
+
+        self.InitUI()
+        self.Centre()
+        self.Show()
+
+    def InitUI(self):
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.text_display = wx.TextCtrl(self, style=wx.TE_RIGHT)
+        vbox.Add(self.text_display, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
+
+        grid_box = wx.GridSizer(5, 4, 10, 10)
+
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+',
+            'C'
+        ]
+
+        for label in buttons:
+            button = wx.Button(self, label=label)
+            self.Bind(wx.EVT_BUTTON, self.OnButtonPress, button)
+            grid_box.Add(button, 1, wx.EXPAND)
+
+        vbox.Add(grid_box, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(vbox)
+
+    def OnButtonPress(self, event):
+        label = event.GetEventObject().GetLabel()
+
+        if label == '=':
+            try:
+                computation = eval(self.text_display.GetValue())
+                self.text_display.SetValue(str(computation))
+            except Exception as e:
+                self.text_display.SetValue("Error")
+        elif label == 'C':
+            self.text_display.SetValue('')
+        else:
+            current_value = self.text_display.GetValue()
+            new_value = current_value + label
+            self.text_display.SetValue(new_value)
+
+if __name__ == '__main__':
+    app = wx.App()
+    Calculator(None, title='Calculator')
+    app.MainLoop()
